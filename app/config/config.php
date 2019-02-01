7a842759e3f4c9cc66becb0ecb9486a251309b58
Yashim Wong <yashimwong@gmail.com>
2019-02-01 19:22:33 +0800

    Added config file
    - For storing common constants like site_name, url and app roots

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+// App Root Directory
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URL_ROOT', 'http://localhost/PHPKubis');
+
+// Site Name
+define('SITE_NAME', 'PHPKubis');
+
+
+
+
+?>
