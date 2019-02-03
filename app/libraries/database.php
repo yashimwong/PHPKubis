7c908f94c0ef22c98a7db3be048a8e4a85218e3f
Yashim Wong <yashimwong@gmail.com>
2019-02-03 21:35:41 +0800

    Updated Database Class
    - Added prepare method (used for preparing queries)
    - Added bind method (used to assign variable type)
    - Added execute method (to run query)
    - Added 2 methods to either return array of objects (resultSet) or return single item as object (single)
    - Added method to display row count

--- app/libraries/database.php
+++ app/libraries/database.php
@@ -37,6 +37,57 @@ class Database {
 
 	}
 
+	// Prepare Statement with query
+	public function query($sql){
+		$this->statement = $this->dbh->prepare($sql);
+	}
+
+	// Bind Values
+	public function bind($param, $value, $type = null){
+		if(is_null($type)){
+			switch(true){
+				case is_int($value):
+					$type = PDO::PARAM_INT;
+					break;
+
+				case is_bool($value):
+					$type = PDO::PARAM_BOOL;
+					break;
+
+				case is_null($value):
+					$type = PDO::PARAM_NULL;
+					break;
+
+				default:
+					$type = PDO::PARAM_STR;
+			}
+		}
+
+		$this->statement->bindValue($param, $value, $type);
+	}
+
+	// Execute the prepared statement
+	public function execute(){
+		return $this->statement->execute();
+	}
+
+	// Get result set as array of objects
+	public function resultSet(){
+		$this->execute();
+		return $this->statement->fetchAll(PDO::FETCH_OBJ);
+	}
+
+	// Get single record as object
+	public function single(){
+		$this->execute();
+		return $this->statement->fetch(PDO::FETCH_OBJ);
+	}
+
+	// Get row count
+	public function rowCount(){
+		return $this->statement->rowCount();
+	}
+
 }
 
 
