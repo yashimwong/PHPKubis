2c14e16a6695e6ce8b3419e8789520d39785424d
Yashim Wong <yashimwong@gmail.com>
2019-02-12 10:47:23 +0800

    Added Bootstrap and Font Awesome Components

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -2,8 +2,15 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.0/css/bootstrap.min.css" integrity="sha384-PDle/QlgIONtM1aqA2Qemk5gPOE7wFq8+Em+G/hmo5Iq0CCmYZLv3fVRDJ4MMwEA" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
 	<link rel="stylesheet" href="<?php echo URL_ROOT; ?>/css/style.css">
 	<title><?php echo SITE_NAME; ?></title>
 </head>
 <body>
+<div class="container">
 	
