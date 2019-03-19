761b3996a91f05bdbcb9bd570f956c90d19527b4
Yashim Wong <yashimwong@gmail.com>
2019-03-19 17:37:33 +0800

    Corrected typo in SESSION variables

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -177,9 +177,9 @@
 
 
 		public function logout() {
-			unset($_SESSOPM['user_id']);
-			unset($_SESSOPM['user_email']);
-			unset($_SESSOPM['user_name']);
+			unset($_SESSION['user_id']);
+			unset($_SESSION['user_email']);
+			unset($_SESSION['user_name']);
 			session_destroy();
 			redirect('users/login');
 		}
